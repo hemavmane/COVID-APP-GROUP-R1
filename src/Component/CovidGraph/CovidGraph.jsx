457410a455a45879aeb41d033ba12a1e3ca20911
hemavmane <hemamane5376@gmail.com>
2022-11-09 16:17:56 +0530

    covidApp

---
+++ src/Component/CovidGraph/CovidGraph.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  AreaChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Area,
+} from "recharts";
+export function MyCovidApp() {
+  const [coviddata, setCovidData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/`,
+        {
+          headers: {
+            "X-RapidAPI-Key":
+              "8c0c5645f4msh17b4e89de7b874ap10eb20jsn3e95f4dad049",
+            "X-RapidAPI-Host":
+              "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com",
+          },
+        }
+      )
+      .then(response => {
+        console.log(response.data);
+        setCovidData([...response.data]);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>COVID CASES & DEATHS</h1>
+      <div>
+        <AreaChart
+          width={730}
+          height={250}
+          data={coviddata}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="Deaths" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="coviddata.TotalCases"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#colorUv)"
+          />
+          <Area
+            type="monotone"
+            dataKey="coviddata.TotalDeaths"
+            stroke="#82ca9d"
+            fillOpacity={1}
+            fill="url(#colorPv)"
+          />
+        </AreaChart>
+      </div>
+    </>
+  );
+}
