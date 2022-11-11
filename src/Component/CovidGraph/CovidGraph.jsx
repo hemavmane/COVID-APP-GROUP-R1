befe4c13d69858bd758d0a7548e4961af0f8acb3
hemavmane <hemamane5376@gmail.com>
2022-11-11 14:52:23 +0530

    change by hema

--- src/Component/CovidGraph/CovidGraph.jsx
+++ src/Component/CovidGraph/CovidGraph.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+// import './CovidGraph.css';
 import {
   AreaChart,
   XAxis,
@@ -8,54 +9,66 @@ import {
   Tooltip,
   Area,
 } from "recharts";
-export function MyCovidApp() {
-  const [coviddata, setCovidData] = useState([]);
 
+
+export function MyCovidApp() {
+  
+  const [coviddata, setCovidData] = useState([]);
+// let obj =[{
+//   country:"india",
+//   TotalCases:"983493",
+//   TotalDeaths:"32490"
+// }]
   useEffect(() => {
     axios
       .get(
-        `https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/`,
-        {
+        `https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/`,{
           headers: {
-            "X-RapidAPI-Key":
-              "8c0c5645f4msh17b4e89de7b874ap10eb20jsn3e95f4dad049",
-            "X-RapidAPI-Host":
-              "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com",
-          },
-        }
-      )
+            'X-RapidAPI-Key': '8c0c5645f4msh17b4e89de7b874ap10eb20jsn3e95f4dad049',
+            'X-RapidAPI-Host': 'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com'
+          }
+        })
       .then(response => {
         console.log(response.data);
+        response.data=response.data.sort()
         setCovidData([...response.data]);
+
+        
       })
       .catch(function (error) {
         console.error(error);
-      });
+      
+    }
+      );
+      console.log(coviddata)
   }, []);
 
+  
   return (
     <>
       <h1>COVID CASES & DEATHS</h1>
-    
-      <div>
-        <AreaChart
-          width={830}
+      <div style={{margin:"100px"}}>
+        <AreaChart  
+          width={1000}
           height={450}
           data={coviddata}
-          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-          <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+          style={{overflow:"visible"}}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0}}>
+          <defs >
+
+
+            {/* <linearGradient  id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8d3484d8" stopOpacity={0.8} />
               <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
             </linearGradient>
             <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
               <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
               <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-            </linearGradient>
+            </linearGradient> */}
           </defs>
-          <XAxis dataKey="Deaths" tickCount="1,2" />
-          <YAxis />
-          <CartesianGrid strokeDasharray="[1,2,3,4,5]" />
+          <XAxis dataKey="Country"  />
+          <YAxis dataKey="Population"  />
+          <CartesianGrid  />
           <Tooltip />
           <Area
             type="monotone"
@@ -76,3 +89,4 @@ export function MyCovidApp() {
     </>
   );
 }
+
