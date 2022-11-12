3d2d45fae4064c08b53c4309e26d62ff34ac3caa
Mohd Ziya <iamziya53@gmail.com>
2022-11-12 19:51:23 +0530

    change by ziya

---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,266 @@
+import React from "react";
+export function FooterApp(){
+    return (
+     <div>     
+    <div id="block" class="subscription main">
+        <h2>Subscribe to our newsletter</h2> 
+    <button class="btn -white js-btn-subscribe-modal">Subscribe</button>
+    <div class="modal subscribe-modal-form js-subscribe-modal-form">
+    <form class="webform-submission-form webform-submission-add-form webform-submission-contact-form webform-submission-contact-add-form webform-submission-contact-node-89211-form webform-submission-contact-node-89211-add-form js-webform-details-toggle webform-details-toggle" data-drupal-selector="webform-submission-contact-node-89211-add-form" action="/covid19/dashboard" method="post" id="webform-submission-contact-node-89211-add-form" accept-charset="UTF-8" data-once="form-updated" data-drupal-form-fields="edit-first-name,edit-last-name,edit-email,edit-company,edit-country,edit-actions-submit,edit-url">
+      <button class="modal__close js-modal__close"></button>
+      
+      <div class="js-form-item form-item js-form-type-textfield form-item-first-name js-form-item-first-name form-no-label">
+        <label for="edit-first-name" class="visually-hidden js-form-required form-required">First Name</label>
+          <input data-webform-required-error="This Fields is Required." data-drupal-selector="edit-first-name" type="text" id="edit-first-name" name="first_name" value="" size="60" maxlength="255" placeholder="First Name" class="form-text required" required="required" aria-required="true">
+    
+          </div>
+    <div class="js-form-item form-item js-form-type-textfield form-item-last-name js-form-item-last-name form-no-label">
+        <label for="edit-last-name" class="visually-hidden js-form-required form-required">Last Name</label>
+          <input data-webform-required-error="This Field is Required." data-drupal-selector="edit-last-name" type="text" id="edit-last-name" name="last_name" value="" size="60" maxlength="255" placeholder="Last Name" class="form-text required" required="required" aria-required="true">
+    
+          </div>
+    <div class="js-form-item form-item js-form-type-email form-item-email js-form-item-email form-no-label">
+        <label for="edit-email" class="visually-hidden js-form-required form-required">Email</label>
+          <input data-webform-required-error="This field is required." data-drupal-selector="edit-email" type="email" id="edit-email" name="email" value="" size="60" maxlength="254" placeholder="name@email.com" class="form-email required" required="required" aria-required="true">
+    
+          </div>
+    <div class="js-form-item form-item js-form-type-textfield form-item-company js-form-item-company form-no-label">
+        <label for="edit-company" class="visually-hidden js-form-required form-required">Company</label>
+          <input data-webform-required-error="This Field is Required." data-drupal-selector="edit-company" type="text" id="edit-company" name="company" value="" size="60" maxlength="255" placeholder="Company" class="form-text required" required="required" aria-required="true">
+    
+          </div>
+    <div class="js-form-item form-item js-form-type-textfield form-item-country js-form-item-country form-no-label">
+        <label for="edit-country" class="visually-hidden js-form-required form-required">Country</label>
+          <input data-webform-required-error="This Field is Required." data-drupal-selector="edit-country" type="text" id="edit-country" name="country" value="" size="60" maxlength="255" placeholder="Country" class="form-text required" required="required" aria-required="true">
+    
+          </div>
+    <input data-drupal-selector="edit-language" type="hidden" name="language" value="English">
+      <div data-drupal-selector="edit-captcha" class="captcha"><input data-drupal-selector="edit-captcha-sid" type="hidden" name="captcha_sid" value="40562831">
+    <input data-drupal-selector="edit-captcha-token" type="hidden" name="captcha_token" value="CYKa_IxFQyrjPV_M-cwjiQUP3SJZy5tG70Zo2BtvCho">
+    <input data-drupal-selector="edit-captcha-response" type="hidden" name="captcha_response" value="Google no captcha">
+    <div class="g-recaptcha" data-sitekey="6LdDJMwZAAAAAMFsKJRlI8L7sACZlVULqjoClv3S" data-theme="light" data-type="image"><div style="width: 304px; height: 78px;"><div><iframe title="reCAPTCHA" src="https://www.recaptcha.net/recaptcha/api2/anchor?ar=1&amp;k=6LdDJMwZAAAAAMFsKJRlI8L7sACZlVULqjoClv3S&amp;co=aHR0cHM6Ly93d3cuZ2F2aS5vcmc6NDQz&amp;hl=en&amp;type=image&amp;v=jF-AgDWy8ih0GfLx4Semh9UK&amp;theme=light&amp;size=normal&amp;cb=nsk2rbpgw00z" width="304" height="78" role="presentation" name="a-89iukqoc6iyt" frameborder="0" scrolling="no" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation allow-modals allow-popups-to-escape-sandbox"></iframe></div><textarea id="g-recaptcha-response" name="g-recaptcha-response" class="g-recaptcha-response" style="width: 250px; height: 40px; border: 1px solid rgb(193, 193, 193); margin: 10px 25px; padding: 0px; resize: none; display: none;"></textarea></div><iframe style="display: none;"></iframe></div><input data-drupal-selector="edit-captcha-cacheable" type="hidden" name="captcha_cacheable" value="1">
+    </div>
+    <div data-drupal-selector="edit-actions" class="form-actions webform-actions js-form-wrapper form-wrapper" id="edit-actions"><input class="webform-button--submit button button--primary js-form-submit form-submit" data-drupal-selector="edit-actions-submit" type="submit" id="edit-actions-submit" name="op" value="Subscribe">
+    
+    </div>
+    <div id="edit-disclaimer" class="js-form-item form-item js-form-type-webform-markup form-item-disclaimer js-form-item-disclaimer form-no-label">
+          <small>By clicking the "Subscribe" button, you are agreeing to receive the digital newsletter from Gavi, the Vaccine Alliance, agreeing to our <a href="/terms-use" target="_blank">terms of use</a> and have read our&nbsp;<a href="/privacy-policy" target="_blank">privacy policy</a>.</small>
+          </div>
+    <input autocomplete="off" data-drupal-selector="form-k9ceokqtrt1e7ci0rzbmbd7bcw7bbrjubsipjp-uwcw" type="hidden" name="form_build_id" value="form-K9CEokqTrT1E7cI0RZbmBd7bcw7bBrjUBsipjP_UWCw">
+    <input data-drupal-selector="edit-webform-submission-contact-node-89211-add-form" type="hidden" name="form_id" value="webform_submission_contact_node_89211_add_form">
+    <div class="url-textfield js-form-wrapper form-wrapper" style="display: none !important;"><div class="js-form-item form-item js-form-type-textfield form-item-url js-form-item-url">
+        <label for="edit-url">Leave this field blank</label>
+          <input autocomplete="off" data-drupal-selector="edit-url" type="text" id="edit-url" name="url" value="" size="20" maxlength="128" class="form-text">
+    
+          </div>
+    </div>
+    </form>
+    </div>
+    </div>
+
+
+
+
+
+
+
+
+    <footer role="footer" class="footer">
+        <div class="ctn-main">
+                        <div class="footer__left">
+                            <div class="region footer-left" id="footer_left">
+        <div id="block-sitebranding-2">
+      
+        
+            <a href="/" rel="home">
+                              <img src="/sites/default/files/Gavi-logo_1b.png" alt="Home">
+              </a>
+          
+    </div>
+    
+      </div>
+    
+                      </div>
+          
+                        <div class="footer__navigation">
+                            <div class="region footer-navigation" id="footer_navigation">
+        <nav role="navigation" aria-labelledby="block-footer-menu" id="block-footer" class="footer__nav">
+                
+      <h2 class="visually-hidden" id="block-footer-menu">Footer</h2>
+      
+    
+            
+            <ul class="level-0 footer">
+                <li>
+            <a href="/our-alliance" data-drupal-link-system-path="node/76146">Our Alliance</a>
+                  </li>
+              <li>
+            <a href="/programmes-impact" data-drupal-link-system-path="node/64261">Programmes &amp; impact</a>
+                  </li>
+              <li>
+            <a href="/investing-gavi" data-drupal-link-system-path="node/64266">Investing in Gavi</a>
+                  </li>
+              <li>
+            <a href="/vaccineswork" data-drupal-link-system-path="node/112476">VaccinesWork</a>
+                  </li>
+              <li>
+            <a href="/news-resources" data-drupal-link-system-path="node/64276">News &amp; resources</a>
+                  </li>
+          </ul>
+        
+    
+    
+      </nav>
+    <nav role="navigation" aria-labelledby="block-countryhub-2-menu" id="block-countryhub-2" class="footer__nav footer__country-hub">
+                
+      <h2 class="visually-hidden" id="block-countryhub-2-menu">Country Hub</h2>
+      
+    
+            
+              <ul>
+              <li>
+            <a href="/our-support/country-portal-covax-collaboration-platform" title="Country portal and COVAX collaboration platform" class="country-hub-link" data-drupal-link-system-path="node/82961">Country portal | COVAX platform</a>
+            
+                    </li>
+            </ul>
+      
+    
+    
+      </nav>
+    <nav role="navigation" aria-labelledby="block-donate-2-menu" id="block-donate-2" class="footer__nav footer__donate">
+                
+      <h2 class="visually-hidden" id="block-donate-2-menu">Donate</h2>
+      
+    
+            
+            <ul class="level-0 donate">
+                <li>
+            <a href="/donate" title="Donate" data-drupal-link-system-path="node/111586">Donate</a>
+                  </li>
+          </ul>
+        
+    
+    
+      </nav>
+    <nav role="navigation" aria-labelledby="block-footernav-menu" id="block-footernav" class="footer__nav">
+                
+      <h2 class="visually-hidden" id="block-footernav-menu">Footer nav</h2>
+      
+    
+            
+            <ul class="level-0 footer-nav">
+                <li>
+            <a href="/our-alliance/work-with-us" title="Careers" data-drupal-link-system-path="node/74811">Careers</a>
+                  </li>
+              <li>
+            <a href="/contact-us" title="Contact" data-drupal-link-system-path="node/76191">Contact</a>
+                  </li>
+              <li>
+            <a href="http://www.gavi.ethicspoint.com/" title="Ethics hotline" target="_blank">Ethics hotline</a>
+                  </li>
+              <li>
+            <a href="https://www.iffim.org/" title="IFFIm" target="_blank">IFFIm</a>
+                  </li>
+              <li>
+            <a href="/privacy-policy" title="Privacy Policy" data-drupal-link-system-path="node/82651">Privacy Policy</a>
+                  </li>
+              <li>
+            <a href="/terms-use" title="Terms of use" data-drupal-link-system-path="node/82646">Terms of use</a>
+                  </li>
+              <li>
+            <a href="/phishing-and-fraud" data-drupal-link-system-path="node/96841">Phishing and fraud</a>
+                  </li>
+          </ul>
+        
+    
+    
+      </nav>
+    
+      </div>
+    
+                      </div>
+          
+                        <div class="footer__right">
+                            <div class="region footer-right" id="footer_right">
+        <div id="block-footer-partners-text" class="footer__copyright">
+      
+        
+            <div class="layout layout--onecol">
+        <div class="layout__region layout__region--content">
+          
+                <div><p>© Gavi 2022</p>
+    </div>
+          
+        </div>
+      </div>
+    
+      </div>
+    <div id="block-footersocial" class="footer__social-links">
+      
+        
+            <div class="layout layout--onecol">
+        <div class="layout__region layout__region--content">
+          
+                <div><ul><li class="facebook"><a href="https://www.facebook.com/GAVI/" rel="noopener noreferrer" target="_blank">Gavi Facebook</a></li>
+        <li class="twitter"><a href="https://twitter.com/gavi" rel="noopener noreferrer" target="_blank">Gavi Twitter</a></li>
+        <li class="linkedin"><a href="https://www.linkedin.com/company/Gavi/" rel="noopener noreferrer" target="_blank">Gavi LinkedIn</a></li>
+    </ul></div>
+          
+        </div>
+      </div>
+    
+      </div>
+    
+      </div>
+    
+                      </div>
+          
+                        <div class="footer__bottom">
+                            <div class="region footer-bottom" id="footer_bottom">
+        <div id="block-footerpartnerstext" class="footer__partners_text">
+      
+        
+            <div class="layout layout--onecol">
+        <div class="layout__region layout__region--content">
+          
+                <div><table><tbody><tr><td valign="top" width="20%">
+                <p><a href="https://www.vaccinesafetynet.org/" target="_blank"><img src="/sites/default/files/background-image/2020/VSN-logo.png"></a></p>
+                </td>
+                <td valign="top" width="20%">
+                <p><a href="https://www.cafamerica.org/vob/" target="_blank"><img src="/sites/default/files/background-image/2021/CAF-International-Validated-Organization.png"></a></p>
+                </td>
+                <td valign="top" width="60%">
+                <p class="text-align-right" style="font-size: 14px; padding-top: 10px;"><em>Our achievements are thanks to the support and expertise of our founding partners</em></p>
+                </td>
+            </tr></tbody></table></div>
+          
+        </div>
+      </div>
+    
+      </div>
+    <div id="block-footerpartnersmenu" class="footer__partners_menu">
+      
+        
+            <div class="layout layout--onecol">
+        <div class="layout__region layout__region--content">
+          
+                <div><p><a href="https://www.who.int/" target="_blank" title="Visit the WHO website"><img alt="" height="100%" src="/sites/default/files/images/who-footer2.png" width="160"></a><a href="https://www.unicef.org/" target="_blank" title="Visit the UNICEF website"><img alt="" height="100%" src="/sites/default/files/images/unicef-footer2.png" width="160"></a><a href="https://www.gatesfoundation.org/" target="_blank" title="Visit the Gates Foundation website"><img alt="" height="100%" src="/sites/default/files/images/gates-foundation-footer2.png" width="160"></a><a href="https://www.worldbank.org/" target="_blank" title="Visit the World Bank website"><img alt="" height="100%" src="/sites/default/files/images/world-bank-footer2.png" width="160"></a></p>
+    </div>
+          
+        </div>
+      </div>
+    
+      </div>
+    
+      </div>
+    
+                      </div>
+              </div>
+        <div class="footer__bg-color">
+    
+        </div>
+      </footer>
+      </div>
+    )
+
+}
