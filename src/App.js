9b026292650027d37b0d03c215fd33df432a8fbc
Admin <admin@Admins-MacBook-Air.local>
2022-11-27 14:39:24 +0530

    final changes by gayatri

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-=======
 import { Dna } from "react-loader-spinner";
 import { Routes, Route } from "react-router-dom";
->>>>>>> ed774de42c9de6798c8c2c25a74e629e5e967746
-
 import { useContext, useEffect } from "react";
-import { Dna } from 'react-loader-spinner'
-import { Routes, Route } from "react-router-dom";
 
 import { createContextApi } from "./Component/ContextApiProvider1/ContextApiProvider1";
 import { VaccineData } from "./Component/VaccineData/Vaccine";
@@ -15,15 +9,8 @@ import { VaccineButtons } from "./Component/VaccineButtonsContainer/VaccineButto
 import { Fda } from "./Component/FdaApprrove/Fda";
 import { Phase1 } from "./Component/Phase1/Phase";
 import { News } from "./Component/NewsCovid/News";
-<<<<<<< HEAD
-import Header from "./Component/Header/Header";
-import Heading from "./Component/Header/Heading";
-import Cart from "./Component/Header/Cart";
 import { ContextApiProvider1 } from "./Component/ContextApiProvider1/ContextApiProvider1";
 import DataTable from './Component/DataTable/DataTable'
-// import Registration from './Component/Registration/Registration'
-=======
->>>>>>> ed774de42c9de6798c8c2c25a74e629e5e967746
 
 import "./App.css";
 import "./Component/VaccineData/Vaccine.css";
@@ -31,10 +18,7 @@ import "./Component/VaccineData/Vaccine.css";
 
 function App() {
   const { coviddata } = useContext(createContextApi);
-<<<<<<< HEAD
   console.log(coviddata)
-=======
->>>>>>> ed774de42c9de6798c8c2c25a74e629e5e967746
 
   useEffect(() => {
     setTimeout(() => {
@@ -65,7 +49,6 @@ function App() {
       <div>
         {coviddata.length > 0 ? (
           <div className="app_background">
-<<<<<<< HEAD
 
             <VaccineButtons />
             <Routes>
@@ -78,13 +61,8 @@ function App() {
               <Route path="/phase1" element={<Phase1 />} />
               <Route path="/news" element={<News />} />
             </Routes>
-            {/* <ContextApi> */}
             <DataTable />
-            {/* <Registration /> */}
-            {/* </ContextApi> */}
-            {/* <Health/>  */}
 
-=======
             <VaccineButtons />
             <Routes>
               <Route path="/" element={<VaccineData />} />
@@ -94,7 +72,6 @@ function App() {
               <Route path="/news" element={<News/>} />
             </Routes>
            
->>>>>>> ed774de42c9de6798c8c2c25a74e629e5e967746
           </div>
         ) : (
           <div className="dna">
--- src/index.js
+++ src/index.js
@@ -1,45 +1,28 @@
-<<<<<<< HEAD
-import React from 'react';
+import * as React from "react";
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
 import {ContextApi} from './Component/DataTable/CreateContext'
 import "./index.css";
 import  App  from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { HeaderContextApiProvider } from "./Component/HeaderContextApi/HeaderContextApi";
-=======
-import * as React from "react";
-import ReactDOM from "react-dom/client";
-import {BrowserRouter} from "react-router-dom"
-import App from "./App";
->>>>>>> ed774de42c9de6798c8c2c25a74e629e5e967746
+// import { HeaderContextApiProvider } from "./Component/HeaderContextApi/HeaderContextApi";
 import { ContextApiProvider1 } from "./Component/ContextApiProvider1/ContextApiProvider1";
-
 import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>  
     <BrowserRouter>
-<<<<<<< HEAD
-      <HeaderContextApiProvider>
+      {/* <HeaderContextApiProvider> */}
         <ContextApiProvider1>
         <ContextApi>
          <App/>
          </ContextApi>
         </ContextApiProvider1>
-      </HeaderContextApiProvider>
+      {/* </HeaderContextApiProvider> */}
     </BrowserRouter>
   </React.StrictMode>
-);
-=======
-     <ContextApiProvider1> 
-      <App />
-    </ContextApiProvider1>
- </BrowserRouter>
-  </React.StrictMode>
+
 );
 
 
->>>>>>> ed774de42c9de6798c8c2c25a74e629e5e967746
 reportWebVitals();
